bbaa4b2c1633e50d7e05e72ccc04ddb591f36932
Stacey Burke <stacey.burke@telus.com>
2022-11-04 08:57:09 -0700

    Use conditionals to tell people about the weather

---
+++ tinkering.js
@@ -0,0 +1,13 @@
+function multiply(a,b){
+  return a*b;
+}
+
+const result = multiply(2,4);
+console.log(result);
+
+var my_string = "Udacity"
+for (var i = 0; i < my_string.length; i++) {
+  console.log(my_string.charCodeAt(i));
+}
+
+'green'=='green'
